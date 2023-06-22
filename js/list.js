801364746cb812867a837b8bfc9b7a2edc51e8ea
diepAu13122001 <91660142+diepAu13122001@users.noreply.github.com>
2023-06-22 10:32:57 +0700

    update

---
+++ js/list.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCy1C86mv6SiJMUlHaqJF9KHJVKocgslBY",
+  authDomain: "adminweb-honghan-jsi17.firebaseapp.com",
+  databaseURL: "https://adminweb-honghan-jsi17-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "adminweb-honghan-jsi17",
+  storageBucket: "adminweb-honghan-jsi17.appspot.com",
+  messagingSenderId: "221372670814",
+  appId: "1:221372670814:web:e6abde4ff07e8c60e7a5cf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
